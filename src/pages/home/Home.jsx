7ed0ce7695cbe565d89444a3fc8df0f3ae713853
hectordll <contactdealh@gmail.com>
2024-09-23 15:20:11 +0200

    📦 NEW: add connect to API

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import './home.css'
+
+// import './../../constants/_base.scss'
+// import './../../constants/_components.scss'
+// import './../../constants/_layouts.scss'
+// import './../../constants/_variables.scss'
+// import './../../constants/main.scss'
+// import './../../constants/menu-page.scss'
+// import './../../constants/mixins.scss'
+
+
+
+export default function Home() {
+    return (
+        <>
+        <main>
+            <h1>test</h1>
+        </main>
+        </>
+    )
+}
