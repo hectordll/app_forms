4c63d3e36d6f1494a7731469a8fbf2a628183960
Mathea <mathea.gaveriaux@gmail.com>
2024-09-27 15:57:09 +0200

    form v4

--- src/pages/userForm/UserForm.jsx
+++ src/pages/userForm/UserForm.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 import './UserForm.css'
 import { Input } from '../../components/forms/Input'
 import { Checkbox } from '../../components/forms/Checkbox'
-//import { Button } from './components/forms/Button'
 
 function UserForm() {
 const [showCheck, setShowCheck] = useState(false);
+const [message, setMessage] = useState('');
+
+const handleSubmit = (event) => {
+  event.preventDefault(); //empêche la page de recharger
+  setMessage("Votre participation a bien été enregistrée");
+}
 
 return (
   
   <div className="container my-3">
     <h1 className='title'>Tenter de gagner 2 places pour le parc Asterix</h1>
     <Form 
+      onSubmit={handleSubmit}
       onShowCheck={setShowCheck}  // Update showCheck based on the checkbox state
       showCheck={showCheck}
     />
+    {message && <p style={{ color: 'green' }}>{message}</p>}
   </div>
   
 );
@@ -24,46 +31,49 @@ function Form ({showCheck, onShowCheck}){
   return (
     <div className="container my-3">
       <div className='form'>
-        <div className='champs'>
-          <div className='FirstName'>
-            <label>
-              Prénom :
-              <Input type='text'/>
-            </label>
+        <form>
+          <div className='champs'>
+            <div className='FirstName'>
+              <label>
+                Prénom :
+                <Input type='text'/>
+              </label>
+            </div>
+            <div className='LastName'>
+              <label>
+                Nom :
+                <Input type='text'/>
+              </label>
+            </div>
+            <div className='mail'>
+              <label>
+                Adresse mail :
+                <Input type='email'/>
+              </label>
+            </div>
+            <div className='tel'>
+              <label>
+                Numéro de télephone :
+                <Input type='tel'/>
+              </label>
+            </div>
           </div>
-          <div className='LastName'>
-            <label>
-              Nom :
-              <Input type='text'/>
-            </label>
-          </div>
-          <div className='mail'>
-            <label>
-              Adresse mail :
-              <Input type='email'/>
-            </label>
-          </div>
-          <div className='tel'>
-            <label>
-              Numéro de télephone :
-              <Input type='tel'/>
-            </label>
-          </div>
-        </div>
-        
-        <div className='check-rules'>
-          <Checkbox 
-            checked={showCheck} 
-            onChange={onShowCheck} 
-            label="Accepter les conditions d'utilisation"
-          /> 
-        </div>
-        <div className='button'>
-          <button disabled={!showCheck}>
-            Envoyer
-          </button>
-        </div>
           
+          <div className='check-rules'>
+            <Checkbox 
+              checked={showCheck} 
+              onChange={onShowCheck} 
+              label="Accepter les conditions d'utilisation"
+            /> 
+          </div>
+          <div className='button'>
+            <button type='submit' disabled={!showCheck}>
+              Envoyer
+            </button>
+            
+          </div>
+        </form>
+        
       </div>
     
     </div>
