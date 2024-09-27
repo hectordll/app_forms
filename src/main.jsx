863ab4ef3deee428ff40c3bb0b9231bfb70a2b0a
Mathea <mathea.gaveriaux@gmail.com>
2024-09-27 14:24:36 +0200

    form v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import App from './App.jsx'
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import UserForm from './pages/userForm/UserForm.jsx'
 
